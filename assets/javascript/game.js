e0413205112e8c7dd572a43a20f2ce52c3b84b51
Cesar <cesssar626@gmail.com>
2017-07-29 15:56:43 -0600

    Added the play/pause feature in JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
-//Creataing an array for the buttons. When more are added, they will be added to this array.
-var topics = ['lakes', 'mountains', 'beaches', 'one+piece', 'berserk', 'overwatch' ];
+//Creating an array for the buttons. When more are added, they will be added to this array.
+var topics = ['lakes', 'mountains', 'beaches', 'one piece', 'berserk', 'full metal alchemist', 'overwatch', 'heroes of the storm' ];
 
 	//Ill be rendering the topics into buttons here. 
 	function createButtons(){
@@ -17,13 +17,15 @@ var topics = ['lakes', 'mountains', 'beaches', 'one+piece', 'berserk', 'overwatc
 
 		bt.text(topics[i]);
 
+		console.log(bt);
+
 		$("#buttons-go-here").append(bt);		
 		};
 	};
  
  	//This will add topics from the form when the button is clicked
  	$("#add-topic").on("click", function(event) {
- 		//Not sure what this does, but adding since it looks important
+ 		//I believe this makes it so the page does not reload.
  		event.preventDefault();
  		//This will grab the text from the for
  		var topicE = $("#topic-input").val().trim();
@@ -36,7 +38,7 @@ var topics = ['lakes', 'mountains', 'beaches', 'one+piece', 'berserk', 'overwatc
  	});
 
 //This will grab the topic button that is seleted and do a search for it. 
-$("button").on("click", function() {
+$("#buttons-go-here").on("click", "button", function() {
 
 	var searchedTerm = $(this).attr("data-search")
 	var queryUrl ="http://api.giphy.com/v1/gifs/search?q=" +
@@ -57,12 +59,45 @@ $("button").on("click", function() {
 		//creating a for loop for the results
 		for (var i = 0; i < results.length; i++) {
 
-			var gifDiv = $("")
+			var gifDiv = $("<div>");
+			var p = $("<p>");
+
+			$("p").text("Rating:" + results[i].rating);
+
+			var topImg = $("<img>");
+
+			topImg.attr("src", results[i].images.fixed_height_small_still.url);
+			topImg.attr("data-still", results[i].images.fixed_height_small_still.url);
+			topImg.attr("data-state", "still");
+			topImg.attr("data-animate", results[i].images.fixed_height_small.url);
+
+			gifDiv.addClass("floater");
+
+			gifDiv.append(p);
+
+			gifDiv.append(topImg);
+
+			$("#gifs-go-here").prepend(gifDiv);
 			
 		}
 
 
 	});
 });
+
+	$("#gifs-go-here").on("click", "img", function(){
+
+		var state = $(this).attr("data-state")
+		var anna = $(this).attr("data-animate")
+
+		if (state === "still"){
+			$(this).attr("src", anna);
+			$(this).attr("data-state", "animated");
+		} else{
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+		}
+	});
+
 
 createButtons();
